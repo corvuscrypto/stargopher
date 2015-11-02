f1b934b9ef136784315aaa9e6d60438234ef5659
corvuscrypto <cmrallen@gmail.com>
2015-11-01 23:50:30 -0800

    Added functionality for before, after, and on handling behavior for packets

--- packet_builder.go
+++ packet_builder.go
@@ -3,29 +3,47 @@ package stargopher
 import "fmt"
 
 //Packet ... this may not be needed...
-type Packet struct {
-	ID            packetType
-	PayloadLength int64
-}
+type Packet interface{}
 
 //These are the maps that will contain the functions that modify
 //the packets and performs actions before or after handling.
 //Only the packetModHandlers should modify server/client sent data
 var beforeHandlers = make(map[packetType][]func())
-var packetModHandlers = make(map[packetType][]func() interface{})
+var packetModHandlers = make(map[packetType][]func(Packet) Packet)
 var afterHandlers = make(map[packetType][]func())
 
 //PacketHandler will build an appropriate packet, then
 //call any associated methods. The variable passthrough will be true if
 //the data is meant to be sent through. Otherwise the packet will be dropped
 func PacketHandler(data []byte, payloadLength int64) ([]byte, bool) {
+	//log for debugging for now
 	fmt.Println(packetType(data[0]).String(), data[1:])
+
+	ptype := packetType(data[0])
+	packet := PacketDecoder(data, payloadLength)
+
+	//first handle the before action if exists
+	for _, f := range beforeHandlers[ptype] {
+		f()
+	}
+
+	//then do the packet modifying functions
+	for _, f := range packetModHandlers[ptype] {
+		packet = f(packet)
+	}
+
+	//then do the after handling functions
+	for _, f := range afterHandlers[ptype] {
+		f()
+	}
+
+	//passthrough for now
 	return data, true
 }
 
 //PacketDecoder is responsible for turning packet data into an easily
 //modifiable struct to be later reencoded for transport
-func PacketDecoder(data []byte, payloadLength int64) interface{} {
+func PacketDecoder(data []byte, payloadLength int64) Packet {
 
 	//isolate the payload
 	payload := data[len(data)-int(payloadLength):]
